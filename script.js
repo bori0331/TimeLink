f30ba3eb706c95b59e09f353fb730b7027ee2a41
bori0331 <uchiborism@gmail.com>
2026-06-24 10:12:14 +0900

    ハンバーガーメニューの半透明アニメーション効果

--- script.js
+++ script.js
@@ -536,7 +536,12 @@ function toggleMenu(){
 const menu =
 document.getElementById("sideMenu");
 
+const overlay =
+document.getElementById("overlay");
+
 menu.classList.toggle("open");
+
+overlay.classList.toggle("show");
 
 }
 
