ab1ac2bc71084fc46f64c4a3c975223f8cddeca4
bori0331 <uchiborism@gmail.com>
2026-06-24 10:37:53 +0900

    ハンバーガーメニューの機能追加修正

--- script.js
+++ script.js
@@ -546,3 +546,19 @@ overlay.classList.toggle("show");
 }
 
 window.toggleMenu = toggleMenu;
+
+function closeMenu(){
+
+const menu =
+document.getElementById("sideMenu");
+
+const overlay =
+document.getElementById("overlay");
+
+menu.classList.remove("open");
+
+overlay.classList.remove("show");
+
+}
+
+window.closeMenu = closeMenu;
